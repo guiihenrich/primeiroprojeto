053a424dd6677a8c8616843e92c90dc2ddbae4fa
˜guiihenrich˜ <guiihenrich@gmail.com>
2023-08-14 20:43:42 -0300

    ajuste error img

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle(`light`)
+  html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
 
-  if (html.classList.contains(`light`)) {
-    img.setAttribute(`src`, `./assets/avatar-light.png`)
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    img.setAttribute(`src`, `./assets/avatar.png`)
+    img.setAttribute("src", "./assets/avatar.png")
   }
   //      EM VEZ DE FAZER TODO ESSE CODIGO EU SUBSTITUI POR !!TOGGLE!!
 }
